05ede0dd84cdc3ad8b83af14e6ee326952d7f33b
horrokotoj <github@horrokotoj.com>
2021-03-23 10:23:50 +0100

    Ändrade add till create

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import argparse
 from connect import cursor, db
 
-# TODO: add --> create to follow CRUD
-def add_product(product_id, product_code, quantity, price):
+def create_product(product_id, product_code, quantity, price):
     sql = ("INSERT INTO products(product_id, product_code, quantity, price) VALUES (%d, %s, %d, %12.2f)")
     cursor.execute(sql, (product_id, product_code, quantity, price))
     db.commit()
@@ -10,21 +9,21 @@ def add_product(product_id, product_code, quantity, price):
     print("Added log {}".format(product_id))
     
 
-def add_supplier(supplier_id, name_t, phone):
+def create_supplier(supplier_id, name_t, phone):
     sql = ("INSERT INTO suppliers(supplier_id, name_t, phone) VALUES (%d, %s, %d)")
     cursor.execute(sql, (supplier_id, name_t, phone))
     db.commit()
     supplier_id = cursor.lastrowid
     print("Added log {}".format(supplier_id))
 
-def add_productSupplier(product_id, supplier_id):
+def create_productSupplier(product_id, supplier_id):
     sql = ("INSERT INTO productSuppliers(product_id, supplier_id) VALUES (%d, %d)")
     cursor.execute(sql, (product_id, supplier_id))
     db.commit()
     productSupplier_id = cursor.lastrowid
     print("Added log {}".format(productSupplier_id))
 
-def add_order(order_id, product_id, quantity):
+def create_order(order_id, product_id, quantity):
     sql = ("INSERT INTO orders(order_id, product_id, quantity) VALUES (%d, %d, %d)")
     cursor.execute(sql, (order_id, product_id, quantity))
     db.commit()
