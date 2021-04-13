650c8c012feb25dfe6a2d96c56b5cf303dd3cef3
maha8298 <marta.haeggstrom.8298@student.uu.se>
2021-04-13 13:34:33 +0200

    Generate static QR-code on personscreen

--- VaccMe/app/screens/PersonQrScreen.js
+++ VaccMe/app/screens/PersonQrScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 import { styleSheets } from '../styleSheets/StyleSheets';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 /**
  * @brief Renders a QR screen
@@ -13,7 +14,31 @@ function PersonQrScreens(props) {
             style={styleSheets.background}
             source={require('../assets/background.jpg')}
         >
-            <SafeAreaView style={styleSheets.safe}></SafeAreaView>
+            <SafeAreaView style={styleSheets.safe}>
+                <QRCode
+                    //QR code value
+                    value="http://google.com/"
+                    //size of QR Code
+                    size={250}
+                    //Color of the QR Code (Optional)
+                    color="black"
+                    //Background Color of the QR Code (Optional)
+                    backgroundColor="white"
+                    //Logo of in the center of QR Code (Optional)
+                    /*logo={{
+                        url:
+                            'https://www.iconfinder.com/icons/2924061/science_shot_srynge_vaccination_vaccine_icon',
+                    }}*/
+                    //Center Logo size  (Optional)
+                    logoSize={30}
+                    //Center Logo margin (Optional)
+                    logoMargin={2}
+                    //Center Logo radius (Optional)
+                    logoBorderRadius={15}
+                    //Center Logo background (Optional)
+                    logoBackgroundColor="white"
+                />
+            </SafeAreaView>
         </ImageBackground>
     );
 }
