f5249ebfbe71f9d67836d079211f29656f68768c
horrokotoj <github@horrokotoj.com>
2021-04-13 14:04:34 +0200

    Deleting case study

--- Case study/add/addProductSuppliers.sql
+++
@@ -1,8 +0,0 @@
-USE storeDB;
-
-INSERT INTO productSuppliers (product_id, supplier_id)
-VALUES 
-    ( 1, 1),
-    ( 2, 2),
-    ( 3, 3),
-    ( 4, 3);
--- Case study/add/addSuppliers.sql
+++
@@ -1,7 +0,0 @@
-USE storeDB;
-
-INSERT INTO suppliers (supplier_id, supplier_name, phone)
-VALUES 
-    ( 1, 'Elgiganten', 0707070),
-    ( 2, 'MediaMarkt', 01601710),
-    ( 3, 'Teknikmagasinet', 11111111);
