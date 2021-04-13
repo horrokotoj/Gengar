0551e9faf3ae31c96b70bf6fec24b76d6ead02b9
maha8298 <marta.haeggstrom.8298@student.uu.se>
2021-04-13 15:28:24 +0200

    fonts in app.js

--- VaccMe/App.js
+++ VaccMe/App.js
@@ -1,8 +1,27 @@
 import * as React from 'react';
 import Navigator from './app/navigators/Navigator';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
-const App = () => {
-    return <Navigator />;
-};
+const getFonts = () =>
+    Font.loadAsync({
+        'oswald-regular': require('./app/assets/fonts/Oswald-Bold.ttf'),
+        'oswald-semibold': require('./app/assets/fonts/Oswald-SemiBold.ttf'),
+        'oswald-bold': require('./app/assets/fonts/Oswald-Bold.ttf'),
+    });
 
-export default App;
+export default function App() {
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    if (fontsLoaded) {
+        return <Navigator />;
+    } else {
+        return (
+            <AppLoading
+                startAsync={getFonts}
+                onFinish={() => setFontsLoaded(true)}
+                onError={console.warn}
+            />
+        );
+    }
+}
