fb6e5e28e94c977dff74a78933595eacc3c4a8ec
horrokotoj <github@horrokotoj.com>
2021-04-22 18:54:22 +0200

    Using fetch to fetch a list of movies from https://reactnative.dev/movies.json

--- VaccMe/app/screens/PersonQrScreen.js
+++ VaccMe/app/screens/PersonQrScreen.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, ImageBackground, SafeAreaView, Text } from 'react-native';
 import { styleSheets } from '../styleSheets/StyleSheets';
 
+const moviesUrl = 'https://reactnative.dev/movies.json';
 
 /**
  * @brief Renders a QR screen
  * @returns A QR screen
  */
-function PersonQrScreens(props) {
+function PersonQrScreens() {
+    const [isLoadingUrl, setLoadingUrl] = useState(true);
+    const [dataUrl, setDataUrl] = useState([]);
+
+    //Fetching our data from the url
+    useEffect(() => {
+        fetch(moviesUrl)
+            .then((response) => response.json())
+            .then((json) => setDataUrl(json.movies))
+            .catch((error) => alert(error))
+            .finally(setLoadingUrl(false));
+    }, []);
+
     return (
         <ImageBackground
             style={styleSheets.background}
             source={require('../assets/background.jpg')}
         >
-            <SafeAreaView style={styleSheets.safe}></SafeAreaView>
+            <SafeAreaView style={styleSheets.safe}>
+                {isLoadingUrl ? (
+                    <Text>Loading url</Text>
+                ) : (
+                    <FlatList
+                        data={dataUrl}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                            <Text>
+                                {item.title}, {item.releaseYear}
+                            </Text>
+                        )}
+                    />
+                )}
+            </SafeAreaView>
         </ImageBackground>
     );
 }
