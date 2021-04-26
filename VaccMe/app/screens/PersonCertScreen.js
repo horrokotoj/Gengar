f4cdd5faaf0dd4259fd8d61d4c1da377d9601c09
horrokotoj <github@horrokotoj.com>
2021-04-26 18:49:14 +0200

    Able to fetch certificates.

--- VaccMe/app/screens/PersonCertScreen.js
+++ VaccMe/app/screens/PersonCertScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     ImageBackground,
     SafeAreaView,
@@ -6,14 +6,34 @@ import {
     TouchableHighlight,
     View,
 } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { styleSheets } from '../styleSheets/StyleSheets';
-
 
 /**
  * @brief Renders a user certificate screen
  * @returns A certificate screen
  */
 function PersonCertScreen() {
+    const [isLoadingUrl, setLoadingUrl] = useState(true);
+    const [dataUrl, setData] = useState([]);
+
+    //Fetching our data from the url
+    useEffect(() => {
+        const getMoviesFromApiAsync = async () => {
+            try {
+                let response = await fetch(
+                    'http://10.0.2.2:8000/userdata/234385785823438578589'
+                );
+                let json = await response.json();
+                setLoadingUrl(false);
+                setData(json.certificates);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getMoviesFromApiAsync();
+    }, []);
     return (
         <ImageBackground
             style={styleSheets.background}
@@ -25,6 +45,19 @@ function PersonCertScreen() {
                 </View>
                 <View style={styleSheets.tabSheet}>
                     <Text style={styleSheets.tabSheetHeader}> Mina intyg </Text>
+                    {isLoadingUrl ? (
+                        <Text>Loading url</Text>
+                    ) : (
+                        <FlatList
+                            data={dataUrl}
+                            keyExtractor={({ id }, index) => id}
+                            renderItem={({ item }) => (
+                                <Text>
+                                    {item.name}, {item.expirationdate}
+                                </Text>
+                            )}
+                        />
+                    )}
                 </View>
                 <View style={styleSheets.filler}>
                     <TouchableHighlight style={styleSheets.touchableHighlight}>
--- VaccMe/app/screens/PersonQrScreen.js
+++ VaccMe/app/screens/PersonQrScreen.js
@@ -2,23 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { FlatList, ImageBackground, SafeAreaView, Text } from 'react-native';
 import { styleSheets } from '../styleSheets/StyleSheets';
 
-const moviesUrl = 'https://reactnative.dev/movies.json';
-
 /**
  * @brief Renders a QR screen
  * @returns A QR screen
  */
 function PersonQrScreens() {
     const [isLoadingUrl, setLoadingUrl] = useState(true);
-    const [dataUrl, setDataUrl] = useState([]);
+    const [dataUrl, setData] = useState([]);
 
     //Fetching our data from the url
     useEffect(() => {
-        fetch(moviesUrl)
-            .then((response) => response.json())
-            .then((json) => setDataUrl(json.movies))
-            .catch((error) => alert(error))
-            .finally(setLoadingUrl(false));
+        const getMoviesFromApiAsync = async () => {
+            try {
+                let response = await fetch(
+                    'https://reactnative.dev/movies.json'
+                );
+                let json = await response.json();
+                setLoadingUrl(false);
+                setData(json.movies);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getMoviesFromApiAsync();
     }, []);
 
     return (
