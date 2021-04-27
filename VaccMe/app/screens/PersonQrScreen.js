64ac9d98b5282be4a189b0e6fd25710af223e8fb
maha8298 <marta.haeggstrom.8298@student.uu.se>
2021-04-27 10:29:38 +0200

    expo qr

--- VaccMe/app/screens/PersonQrScreen.js
+++ VaccMe/app/screens/PersonQrScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
-import QRCode from 'react-native-qrcode-svg';
+//import QRCode from 'react-native-qrcode-svg';
 import { styleSheets } from '../styleSheets/StyleSheets';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { QRCode } from 'react-native-custom-qr-codes-expo';
 
 /**
  * @brief Renders a QR screen
@@ -15,29 +16,7 @@ function PersonQrScreen(props) {
             source={require('../assets/background.jpg')}
         >
             <SafeAreaView style={styleSheets.safe}>
-                <QRCode
-                    //QR code value
-                    value="http://google.com/"
-                    //size of QR Code
-                    size={250}
-                    //Color of the QR Code (Optional)
-                    color="black"
-                    //Background Color of the QR Code (Optional)
-                    backgroundColor="white"
-                    //Logo of in the center of QR Code (Optional)
-                    /*logo={{
-                        url:
-                            'https://www.iconfinder.com/icons/2924061/science_shot_srynge_vaccination_vaccine_icon',
-                    }}*/
-                    //Center Logo size  (Optional)
-                    logoSize={30}
-                    //Center Logo margin (Optional)
-                    logoMargin={2}
-                    //Center Logo radius (Optional)
-                    logoBorderRadius={15}
-                    //Center Logo background (Optional)
-                    logoBackgroundColor="white"
-                />
+                <QRCode content="https://reactnative.com" />
             </SafeAreaView>
         </ImageBackground>
     );
