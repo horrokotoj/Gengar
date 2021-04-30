052932a441735d180c09b3704788d2b7df2e5243
horrokotoj1337 <60961478+horrokotoj1337@users.noreply.github.com>
2021-04-30 19:12:40 +0200

    PersonCalendarScreen shows certificates sorted on expirationdate from securestore. A network request will not be done until restarted or onPress on updatebutton. PersonCertScreen shows certificates sorted on name fro secure store. A network request will not be done until restarted or onPress on updatebutton. Navigator uses UpdateCertificates to fetch certificats from server on login or if the app was terminated. DeleteItems and StoreItems are used to abstract storing and deleting items from secure store on sign in or sign out.

---
+++ VaccMe/app/network/UpdateCertificates.js
@@ -0,0 +1,28 @@
+import * as SecureStore from 'expo-secure-store';
+
+/**
+ * @breif Updates a users certificates
+ * @param userID of the user to fetch certificates for.
+ */
+async function UpdateCertificates(userId) {
+    let response;
+    try {      
+        response = await fetch(
+            'http://127.0.0.1:8000/userdata', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    googleuserid: "" + userId
+                }),
+                });
+        let json = await response.json();
+        await SecureStore.setItemAsync('userCert', json);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default UpdateCertificates;
